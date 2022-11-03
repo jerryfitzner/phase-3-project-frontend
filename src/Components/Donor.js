12c24459de64c4c8117c04042132eae5e208f442
Jerry Fitzner <jfitzner1@gmail.com>
2022-11-03 10:37:42 -0400

    PATCH Request Added

--- src/Components/Donor.js
+++ src/Components/Donor.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 function Donor ({donor, onDeleteDonor}) {
+  const [isDonate, setIsDonate] = useState(donor.donation_received)
+
+  
 
   const handleDelete = () => {
     fetch(`http://localhost:9292/donors/${donor.id}`, {
@@ -11,23 +14,33 @@ function Donor ({donor, onDeleteDonor}) {
     .then(deletedDonor => onDeleteDonor(deletedDonor.id))
   }
 
-
-
-
+  const handleClick = () => {
+    fetch(`http://localhost:9292/donors/${donor.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "donation_received": !isDonate
+      })
+    })
+    .then(r => r.json())
+    .then(data => setIsDonate(data.donation_received))
+  }
 
   return(
     
       <tr key={uuidv4()}>
-                    <td>{donor.first_name} {donor.last_name}</td>
-                    <td>${donor.donation}</td>
-                    <td>
-                      <button className='Button2'>Yes/No</button>
-                    </td>
-                    <td>
-                      <button className='Delete-button' onClick={handleDelete} id={donor.id}>X</button>
-                    </td>
-                  </tr>
-    
+        <td>{donor.first_name} {donor.last_name}</td>
+        <td>${donor.donation}</td>
+        <td>
+          <button className={isDonate ? "Button2" : "Button3"} onClick={handleClick}>{isDonate ? "Yes" : "No"}</button>
+        </td>
+        <td>
+          <button className='Delete-button' onClick={handleDelete} id={donor.id}>X</button>
+        </td>
+       </tr>
   )
 }
 
